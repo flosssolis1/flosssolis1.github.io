478972816ecbb81dff2a3a29a69611bd792874e0
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-11-26 17:07:59 +0200

    main page

---
+++ losers-games/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={
+            "https://www.meme-arsenal.com/memes/2479e1331c65964f70094494ef5c7445.jpg"
+          }
+          width={"200px"}
+          alt="fat angry cat"
+        />
+        <p>ты лох</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
