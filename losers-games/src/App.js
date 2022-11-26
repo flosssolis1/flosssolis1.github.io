5357e7eb4f34a15aec052131835a28d19174e1ed
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-11-26 17:24:10 +0200

    up

--- losers-games/src/App.js
+++ losers-games/src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
+        {/* <img
           src={
             "https://www.meme-arsenal.com/memes/2479e1331c65964f70094494ef5c7445.jpg"
           }
           width={"200px"}
           alt="fat angry cat"
-        />
+        /> */}
         <p>ты лох</p>
       </header>
     </div>
